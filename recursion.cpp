42abbaa8df47cc8dab6ebad07f1ba1c526c682d7
Your Name <your@email.com>
2025-07-21 14:39:50 +0530

    recursion2

--- recursion.cpp
+++ recursion.cpp
@@ -150,7 +150,123 @@ int countSubSumK(string &s, int i, int sum, int k) {
     return left + right;
 }
 
+void helperComb(int i,int target,vector<int>&candidates,vector<int>&curr,vector<vector<int>>& res){
+        if(i==candidates.size()){
+            if(target==0) {
+                res.push_back(curr);
+            }
+            return;
+        }
+        if(candidates[i]<=target){
+            curr.push_back(candidates[i]);
+            helperComb(i,target-candidates[i],candidates,curr,res);
+            curr.pop_back();
+        }
+            helperComb(i+1,target,candidates,curr,res);
+}
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> curr;
+        helperComb(0,target,candidates,curr,res);
+        return res;
+}
 
+void helperComb2(int i,int target,vector<int>&candidates,vector<int>&curr,vector<vector<int>>& res){
+        if(target==0) {
+                res.push_back(curr);
+        }
+        for(int j=i;j<candidates.size();j++){
+            if(j>i && candidates[j]==candidates[j-1]) continue;
+            if(candidates[i]>target) break;
+            curr.push_back(candidates[j]);
+            helperComb2(j+1,target-candidates[j],candidates,curr,res);
+            curr.pop_back();
+
+        }
+
+            
+        
+}
+ vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> curr;
+        sort(candidates.begin(),candidates.end());
+        helperComb2(0,target,candidates,curr,res);
+        return res;
+}
+
+
+void subsetSumsHelper(int ind, vector < int > & arr, int n, vector < int > & ans, int sum) {
+      if (ind == n) {
+        ans.push_back(sum);
+        return;
+      }
+      subsetSumsHelper(ind + 1, arr, n, ans, sum + arr[ind]);
+      subsetSumsHelper(ind + 1, arr, n, ans, sum);
+}
+vector < int > subsetSums(vector < int > arr, int n) {
+    vector < int > ans;
+    subsetSumsHelper(0, arr, n, ans, 0);
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+
+void findSubs(int ind,vector<int>&nums,vector<int>&ds,vector<vector<int>> &ans){
+        ans.push_back(ds);
+        for(int i=ind;i<nums.size();i++){
+            if (i > ind && nums[i] == nums[i - 1]) continue;
+            ds.push_back(nums[i]);
+            findSubs(i+1,nums,ds,ans);
+            ds.pop_back();
+        }
+}
+vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(nums.begin(),nums.end());
+        findSubs(0,nums,ds,ans);
+        return ans;
+}
+void helperComb3(int start,int k,int n,vector<int>&path,vector<vector<int>> &res){
+        if(k==0&&n==0){
+            res.push_back(path);
+            return;
+        }
+        if(k==0|| n<=0) return;
+        for(int i=start;i<=9;i++){
+            path.push_back(i);
+            helperComb3(i+1,k-1,n-i,path,res);
+            path.pop_back();
+        }
+}
+vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> res;
+        vector<int> path;
+        helperComb3(1,k,n,path,res);
+        return res;
+        
+}
+
+void helperLetter(string &digits,int idx,string&path,vector<string>&res,vector<string>&mapping){
+        if(idx==digits.size()){
+            res.push_back(path);
+            return;
+        }
+        string letters= mapping[digits[idx]-'0'];
+        for(char ch:letters){
+            path.push_back(ch);
+            helperLetter(digits, idx + 1, path, res, mapping);
+            path.pop_back();
+        }
+}
+vector<string> letterCombinations(string digits) {
+        if(digits.empty()) return {};
+        vector<string> mapping={"","","abc","def","ghi","jkl","mno","pqrs", "tuv","wxyz"};
+        vector<string>res;
+        string path;
+        helperLetter(digits,0,path,res,mapping);
+        return res;
+}
 
 int main() {
 
